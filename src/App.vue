b08c877d50a9c9b34c273671ea360bd000637daf
ginkgo <alexvid33@gmail.com>
2022-05-03 12:50:55 +0200

    adjust styles in SelectCategory component
    Co-authored-by: alesweb <aleswebgit@users.noreply.github.com>
    Co-authored-by: Scarlet Rosa <skyrosa@users.noreply.github.com>
    Co-authored-by: LvL090 <LvL090@users.noreply.github.com>

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,14 @@
 
 <template>
   <div class="font-pop">
-    <SelectCategory name="sports" />
+    <main class="p-6 flex flex-col gap-2">
+      <SelectCategory name="science and nature" />
+      <SelectCategory name="history" />
+      <SelectCategory name="sports" />
+      <SelectCategory name="entertainment" />
+      <SelectCategory name="art and literature" />
+      <SelectCategory name="geography" />
+      <SelectCategory name="random" />
+    </main>
   </div>
 </template>
