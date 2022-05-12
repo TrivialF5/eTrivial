daf18bfa96c1290a447d61abaea820de57c770f9
Ales <aleswebgit@gmail.com>
2022-05-12 16:30:54 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   /* alias: {
     '@': path.resolve(__dirname, './src')
   }, */
+  base: '/eTrivial/',
   plugins: [vue()],
   server: {
     host: '0.0.0.0'
--- vue.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  publicPath: 'trivialf5.github.io/eTrivial/'
-}
